0cd1612b81bc6275178847f9fa8110cd8814fbe9
Veer-Dursun <Vdursun.student2025@poly.ac.mu>
2025-10-22 22:19:37 +0400

    Fix URL conflict: remove duplicate root path blocking machine app

--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("Vending Machine App is running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
-    path('', include('machine.urls')),  # Make sure this line exists
+    path('', include('machine.urls')),  # ← ONLY THIS FOR ROOT PATH
 ]
